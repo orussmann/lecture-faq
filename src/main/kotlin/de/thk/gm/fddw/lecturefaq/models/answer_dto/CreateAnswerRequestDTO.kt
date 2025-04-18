f7e65e3cbd981847915b1697552df0f3286099e7
Oliver Russmann <oliver.russmann@smail.th-koeln.de>
2025-04-18 22:02:57 +0200

    Update poll ID to solve the chicken-and-egg problem (HTML controller -> GET)

--- src/main/kotlin/de/thk/gm/fddw/lecturefaq/models/answer_dto/CreateAnswerRequestDTO.kt
+++ src/main/kotlin/de/thk/gm/fddw/lecturefaq/models/answer_dto/CreateAnswerRequestDTO.kt
@@ -3,6 +3,6 @@ package de.thk.gm.fddw.lecturefaq.models.answer_dto
 import java.util.UUID
 
 data class CreateAnswerRequestDTO(
-    val pollId: UUID?,
+    val pollId: UUID? = null,
     val text: String
 )
