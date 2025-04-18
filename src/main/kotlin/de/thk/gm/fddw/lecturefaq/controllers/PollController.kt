489ab7dc45d6f6472f9b454caa20d17f560e2228
Oliver Russmann <oliver.russmann@smail.th-koeln.de>
2025-04-18 22:05:29 +0200

    Add poll controller and templates to serve HTML

---
+++ src/main/kotlin/de/thk/gm/fddw/lecturefaq/controllers/PollController.kt
@@ -0,0 +1,141 @@
+package de.thk.gm.fddw.lecturefaq.controllers
+
+import de.thk.gm.fddw.lecturefaq.constants.MINIMUM_ANSWERS_COUNT
+import de.thk.gm.fddw.lecturefaq.models.answer_dto.CreateAnswerRequestDTO
+import de.thk.gm.fddw.lecturefaq.models.poll_dtos.CreatePollRequestDTO
+import de.thk.gm.fddw.lecturefaq.models.poll_dtos.PollResponseDTO
+import de.thk.gm.fddw.lecturefaq.models.poll_dtos.UpdatePollRequestDTO
+import de.thk.gm.fddw.lecturefaq.services.PollsService
+import de.thk.gm.fddw.lecturefaq.services.UsersServiceImpl
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.ResponseStatusException
+import java.util.UUID
+
+
+@Controller
+@RequestMapping(produces = [MediaType.TEXT_HTML_VALUE])
+class PollsController(
+    private val pollsService: PollsService,
+    private val usersService: UsersServiceImpl
+) {
+
+    @GetMapping("/users/{userId}/polls")
+    @ResponseStatus(HttpStatus.OK)
+    fun getAllPollsFromUser(
+        @PathVariable userId: UUID,
+        model: Model
+    ): String {
+        try {
+            val usersPolls = pollsService.findAllByUserId(userId)
+            model.addAttribute("polls", usersPolls)
+            model.addAttribute("userId", userId)
+            return "polls/showPolls"
+        } catch (e: Exception) {
+            throw ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Could not fetch polls")
+        }
+    }
+
+    //TODO: Consider removing this
+    /*@GetMapping("/polls")
+    @ResponseStatus(HttpStatus.OK)
+    fun getAllPolls(): MutableIterable<PollResponseDTO> {
+        try {
+            val allPolls = pollsService.findAll()
+            return allPolls
+        } catch (e: Exception) {
+            throw ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Could not fetch polls")
+        }
+    }*/
+
+    @PostMapping("/users/{userId}/polls")
+    fun createPoll(
+        @PathVariable userId: UUID,
+        @RequestParam title: String,
+        @RequestParam description: String,
+        @RequestParam answer1: String,
+        @RequestParam answer2: String,
+        @RequestParam answer3: String,
+        @RequestParam answer4: String,
+        @RequestParam answer5: String
+    ): String {
+        try {
+            val answers = listOf(answer1, answer2, answer3, answer4, answer5)
+                .filter { it.isNotBlank() }
+                .map { answer ->  CreateAnswerRequestDTO(text = answer) }
+            if (answers.size < MINIMUM_ANSWERS_COUNT) {
+                throw ResponseStatusException(HttpStatus.BAD_REQUEST, "Minimum 2 answers expected")
+            }
+            val poll = CreatePollRequestDTO(
+                userId = userId,
+                title = title,
+                description = description,
+                answers = answers
+            )
+            val createdPoll = pollsService.save(poll)
+            return "redirect:/app/users/${userId}/polls"
+        } catch (e: ResponseStatusException) {
+            throw e
+        } catch (e: Exception) {
+            throw ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Could not create poll")
+        }
+    }
+
+
+    //TODO: Get mapping for one poll
+    @GetMapping("/users/{userId}/polls/{pollId}")
+    fun getPollById(
+        @PathVariable pollId: UUID,
+        @PathVariable userId: String,
+        model: Model
+    ): String {
+        try {
+            val poll = pollsService.findById(pollId)
+            model.addAttribute("poll", poll)
+            return "polls/showPoll"
+        } catch (e: Exception) {
+            throw ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Could not fetch poll")
+        }
+    }
+
+    @DeleteMapping("/users/{userId}/polls/{pollId}")
+    fun deletePoll(
+        @PathVariable pollId: UUID,
+        @PathVariable userId: UUID
+    ): String {
+        try {
+            pollsService.removeById(pollId)
+            return "redirect:/app/users/${userId}/polls"
+        } catch (e: Exception) {
+            throw ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Could not delete poll")
+        }
+    }
+
+    @PutMapping("/users/{userId}/polls/{pollId}")
+    fun updatePoll(
+        @PathVariable pollId: UUID,
+        @PathVariable userId: UUID,
+        @RequestParam title: String,
+        @RequestParam description: String
+    ): String {
+        try {
+            val poll = UpdatePollRequestDTO(title, description)
+            val updatedPoll = pollsService.updateById(pollId, poll)
+            return "redirect:/app/users/${userId}/polls"
+        } catch (e: Exception) {
+            throw ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Could not update poll")
+        }
+    }
+}
